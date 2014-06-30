0f8621f9177f87e91fcc7538e0a36a3e2c8b5cc9
Chadwick Flores <chadwickflores@gmail.com>
2014-06-29 23:06:57 -0500

    add new todo items, template, form, integration test, and controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,4 +2,13 @@ class TodoItemsController < ApplicationController
   def index
   	@todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def new
+  	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+  	
+  end
 end
