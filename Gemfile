34f2b1c1fb2874321acd04fe5c4ea1c32bf7eb7d
Chadwick Flores <chadwickflores@gmail.com>
2014-07-10 18:05:11 -0500

    authentication spec, no integration tests

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.1.0'
+gem 'bcrypt-ruby', '~> 3.1.2'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe User do
+  let(:valid_attributes) {
+  	{ 
+  		first_name: "Chad",
+  		last_name: "Flores",
+  		email: "chadwickflores@gmail.com",
+      password: "treehouse1234",
+      password_confirmation: "treehouse1234"
+  	}
+  }
+  context "validations" do
+  	let(:user) { User.new(valid_attributes) }	
+  	
+  	before do
+  		User.create(valid_attributes)
+  	end
+
+  	it "requires and email" do
+  		expect(user).to validate_presence_of(:email)
+  	end
+
+  	it "requires a unique email" do
+  		expect(user).to validate_uniqueness_of(:email)
+  	end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "CHADWICKFLORES@GMAIL.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires the email address to look like an email" do
+      user.email = "chad"
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe "#downcase_email" do
+  	it "makes the email attribute lower case" do
+  		user = User.new(valid_attributes.merge(email: "CHADWICKFLORES@GMAIL.COM"))
+  		#user.downcase_email
+  		#expect(user.email).to eq("chadwickflores@gmail.com")
+      expect{ user.downcase_email }.to change{ user.email }.
+        from("CHADWICKFLORES@GMAIL.COM").
+        to("chadwickflores@gmail.com")
+  	end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "CHADWICKFLORES@GMAIL.COM"
+      expect(user.save).to be_true
+      expect(user.email).to eq("chadwickflores@gmail.com")
+    end
+  end
+end
