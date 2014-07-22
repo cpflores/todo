812ad6dd917fd487638850c7d427c042b9f2dffb
Chadwick Flores <chadwickflores@gmail.com>
2014-07-22 10:16:55 -0500

    remove description from todo list and fix specs

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -5,14 +5,12 @@ describe "Creating todo lists" do
 
 	def create_todo_list(options={})
 		options[:title] ||= "My todo list"
-		options[:description] ||= "This is my todo list"
 
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
 		fill_in "Title", with: options[:title]
-		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
 	end
 
@@ -42,30 +40,6 @@ describe "Creating todo lists" do
 
 		create_todo_list title: "Hi"
 		
-		expect(page).to have_content("error")
-		expect(TodoList.count).to eq(0)
-
-		visit "/todo_lists"
-		expect(page).to_not have_content("This is what I'm doing today.")
-	end
-
-	it "displays an error when the todo list has no description" do
-		expect(TodoList.count).to eq(0)
-
-		create_todo_list title: "Grocery List", description: ""
-		
-		expect(page).to have_content("error")
-		expect(TodoList.count).to eq(0)
-
-		visit "/todo_lists"
-		expect(page).to_not have_content("This is what I'm doing today.")
-	end
-
-	it "displays an error when the todo list has a description less than 5 characters" do
-		expect(TodoList.count).to eq(0)
-
-		create_todo_list title: "Grocery List", description: "Food"
-
 		expect(page).to have_content("error")
 		expect(TodoList.count).to eq(0)
 
